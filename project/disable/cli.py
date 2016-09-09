9ba9fc3cefa3f8f47b9c551166074d96ade1ac93
Chunlei Wu <anewgene@yahoo.com>
2016-09-09 13:47:34 -0700

    add disable/enable for touchpoint; remove touchscreen handling.

--- project/disable/cli.py
+++ project/disable/cli.py
@@ -3,7 +3,9 @@ import os
 
 
 TRACKPAD_COMMAND = "xinput disable 'SynPS/2 Synaptics TouchPad'"
-TOUCH_COMMAND = "xinput disable 'ELAN Touchscreen'"
+TRACKPOINT_COMMAND = "xinput disable 'TPPS/2 IBM TrackPoint'"
+#TOUCH_COMMAND = "xinput disable 'ELAN Touchscreen'"
+TOUCH_COMMAND = "echo \"No touchscreen to handle.\""
 
 
 @click.command('disable', short_help='Disable component.')
@@ -12,6 +14,7 @@ def cli(component):
     exit_code = 0
     if component in ['trackpad', 'all']:
         exit_code = os.system(TRACKPAD_COMMAND)
+        exit_code = os.system(TRACKPOINT_COMMAND)
 
     if exit_code == 0 and component in ['touch', 'all']:
         exit_code = os.system(TOUCH_COMMAND)
--- project/enable/cli.py
+++ project/enable/cli.py
@@ -3,8 +3,9 @@ import os
 
 
 TRACKPAD_COMMAND = "xinput enable 'SynPS/2 Synaptics TouchPad'"
-TOUCH_COMMAND = "xinput enable 'ELAN Touchscreen'"
-
+TRACKPOINT_COMMAND = "xinput enable 'TPPS/2 IBM TrackPoint'"
+#TOUCH_COMMAND = "xinput enable 'ELAN Touchscreen'"
+TOUCH_COMMAND = "echo \"No touchscreen to handle.\""
 
 @click.command('enable', short_help='Enable component.')
 @click.argument('component', default='all', type=click.Choice(['trackpad', 'touch', 'all']))
@@ -12,6 +13,7 @@ def cli(component):
     exit_code = 0
     if component in ['trackpad', 'all']:
         exit_code = os.system(TRACKPAD_COMMAND)
+        exit_code = os.system(TRACKPOINT_COMMAND)
 
     if exit_code == 0 and component in ['touch', 'all']:
         exit_code = os.system(TOUCH_COMMAND)
